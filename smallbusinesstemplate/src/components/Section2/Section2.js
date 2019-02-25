111cb617da6262748b27c3a52653a7defd37a947
davidkavanaugh <davidkavanaugh@outlook.com>
2019-02-25 12:51:08 -0800

    aligning items

--- smallbusinesstemplate/src/components/Section2/Section2.js
+++ smallbusinesstemplate/src/components/Section2/Section2.js
@@ -11,28 +11,29 @@ class Section2 extends Component {
   render() {
     return (
       <div id="section_2" className="fullHeight">
-      <section>
-        <Hidden xsDown>
-          <div className="column">
-            <br /><br /><br /><img src={photo} alt="description" className="section2__photo1"/>
-          </div>
-        </Hidden>
-        <div className="column"><h3 className="section2__title">Section 2</h3>
-          <Hidden smUp>
-            <img src={photo} alt="description" className="section2__photo1"/>
+        <section>
+          <Hidden xsDown>
+            <div className="column">
+              <img src={photo} alt="description" className="section2__photo1"/>
+            </div>
           </Hidden>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Arcu non odio euismod lacinia at quis risus sed. Neque sodales ut etiam sit amet nisl purus. Enim facilisis gravida neque convallis a cras semper auctor neque. Fringilla urna porttitor rhoncus dolor purus non enim praesent. Id neque aliquam vestibulum morbis.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Arcu non odio euismod lacinia at quis risus sed. Neque sodales ut etiam sit amet nisl purus. Enim facilisis gravida neque convallis a cras semper auctor neque. Fringilla urna porttitor rhoncus dolor purus non enim praesent. Id neque aliquam vestibulum morbis.
-          </p>
-          <h4 className="phonenumber lineHeight">(555) 555-5555</h4>
-          <h4 className="emailaddress lineHeight">email address</h4> 
-          <div className="social">
-            <InstagramBtn />
-            <FacebookBtn />
-          </div>
+          <div className="column">
+            <h3 className="section2__title">Section 2</h3>
+            <Hidden smUp>
+              <img src={photo} alt="description" className="section2__photo1"/>
+            </Hidden>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Arcu non odio euismod lacinia at quis risus sed. Neque sodales ut etiam sit amet nisl purus. Enim facilisis gravida neque convallis a cras semper auctor neque. Fringilla urna porttitor rhoncus dolor purus non enim praesent. Id neque aliquam vestibulum morbis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Arcu non odio euismod lacinia at quis risus sed. Neque sodales ut etiam sit amet nisl purus. Enim facilisis gravida neque convallis a cras semper auctor neque. Fringilla urna porttitor rhoncus dolor purus non enim praesent. Id neque aliquam vestibulum morbis.
+            </p>
+            <h4 className="phonenumber lineHeight">(555) 555-5555</h4>
+            <h4 className="emailaddress lineHeight">email address</h4> 
+            <div className="social">
+              <InstagramBtn />
+              <FacebookBtn />
+            </div>
         </div>
           
       </section>
